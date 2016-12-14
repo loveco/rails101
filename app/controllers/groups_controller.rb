f18942af047e9cac5d5116c63f2a75b527c5289d
chunxiao <lvbanyao@gmail.com>
2016-12-14 17:30:09 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,8 +11,9 @@ class GroupsController < ApplicationController
    @group = Group.new
  end
 
-  def show
+ def show
    @group = Group.find(params[:id])
+   @posts = @group.posts
  end
 
  def edit
